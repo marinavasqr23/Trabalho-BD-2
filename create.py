3f396038781adf62a5b565818a04d7a2ea99d2fc
Marina Vasques Rodrigues <marinavasqufc@gmail.com>
2025-01-29 14:59:16 -0300

    create e drope das tabelas

--- create.py
+++ create.py
@@ -1,13 +1,100 @@
-# Importando Biblioteca
-import psycopg2
+import psycopg
 
 # Conectando Python ao PostgreSQL
-con = psycopg2.connect(
+con = psycopg.connect(
     host='200.129.44.249',   # Endereço IP do servidor PostgreSQL
-    database='496500',       # Nome do banco de dados
+    dbname='496500',         # Nome do banco de dados
     user='496500',           # Nome de usuário
     password='496500'        # Senha
 )
 
 # Criando um cursor
 cur = con.cursor()
+
+# Executando o comando CREATE TABLE para as Tabelas Curso, Aluno, Professor, Disciplina, Turma e Aluno_Turma.
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.curso (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        regime VARCHAR(100) NOT NULL,
+        duracao INT NOT NULL
+    );
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.aluno (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        curso_id INT NOT NULL,
+        semestre INT NOT NULL,
+        FOREIGN KEY (curso_id) REFERENCES projetobancodedados.curso(id)
+    );
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.aluno (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        curso_id INT NOT NULL,
+        semestre INT NOT NULL,
+        FOREIGN KEY (curso_id) REFERENCES projetobancodedados.curso(id)
+    );
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.professor (
+        id INT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        area_especializacao VARCHAR(100) NOT NULL,
+        contato VARCHAR(100),
+        curso_id INT,
+        FOREIGN KEY (curso_id) REFERENCES projetobancodedados.curso(id)
+    );
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.disciplina (
+        id INT PRIMARY KEY,
+        codigo VARCHAR(10) UNIQUE NOT NULL,
+        nome VARCHAR(100) NOT NULL,
+        area_especializacao VARCHAR(100) NOT NULL,
+        carga_horaria INT NOT NULL,
+        curso_id INT,
+        FOREIGN KEY (curso_id) REFERENCES projetobancodedados.curso(id)
+    );
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.turma (
+        id INT PRIMARY KEY,
+        codigo VARCHAR(10) UNIQUE NOT NULL,
+        disciplina_id INT,
+        semestre VARCHAR(20) NOT NULL,
+        capacidade_maxima INT NOT NULL,
+        estado VARCHAR(20) NOT NULL,
+        prof_id INT,
+        FOREIGN KEY (disciplina_id) REFERENCES projetobancodedados.disciplina(id),
+        FOREIGN KEY (prof_id) REFERENCES projetobancodedados.professor(id)
+    );
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS projetobancodedados.aluno_turma (
+        aluno_id INT,
+        turma_id INT,
+        PRIMARY KEY (aluno_id, turma_id),
+        FOREIGN KEY (aluno_id) REFERENCES projetobancodedados.aluno(id),
+        FOREIGN KEY (turma_id) REFERENCES projetobancodedados.turma(id)
+    );
+""")
+
+
+# Confirmando a operação
+con.commit()
+
+# Exibindo a mensagem de sucesso
+print("Tabelas criadas com sucesso!")
+
+# Fechando a conexão
+cur.close()
+con.close()
--- drope.py
+++ drope.py
@@ -1,10 +1,10 @@
 # Importando Biblioteca
-import psycopg2
+import psycopg
 
 # Conectando Python ao PostgreSQL
-con = psycopg2.connect(
+con = psycopg.connect(
     host='200.129.44.249',   # Endereço IP do servidor PostgreSQL
-    database='496500',       # Nome do banco de dados
+    dbname='496500',       # Nome do banco de dados
     user='496500',           # Nome de usuário
     password='496500'        # Senha
 )
@@ -12,17 +12,30 @@ con = psycopg2.connect(
 # Criando um cursor
 cur = con.cursor()
 
-# Executando o comando DROP TABLE na tabelas e nas tabelas associadas
-# cur.execute("DROP TABLE IF EXISTS projetobancodedados.turma_aluno")
-# cur.execute("DROP TABLE IF EXISTS projetobancodedados.turma_disciplina")
-# cur.execute("DROP TABLE IF EXISTS projetobancodedados.projeto_aluno")
-# cur.execute("DROP TABLE IF EXISTS projetobancodedados.avaliacao_turma")
-# cur.execute("DROP TABLE IF EXISTS projetobancodedados.nota")
-# cur.execute("DROP TABLE IF EXISTS projetobancodedados.avaliacao")
+# Executando o comando DROP TABLE na tabelas e nas tabelas associadas que precisam ser excluídas
+
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.turma_aluno")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.turma_disciplina")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.projeto_aluno")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.avaliacao_turma")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.nota")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.avaliacao")
 cur.execute("DROP TABLE IF EXISTS projetobancodedados.aluno")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.turma_sala")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.professor_turma")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.material_turma")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.projeto_professor")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.disciplina_material")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.materia_professor")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.aluno")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.professor")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.disciplina")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.turma")
+cur.execute("DROP TABLE IF EXISTS projetobancodedados.curso")
+
 
 # Confirmando a operação
-con.commit()  # Isso garante que a operação de exclusão seja confirmada no banco
+con.commit()  
 
 # Exibindo a mensagem de sucesso
 print("Tabela excluída com sucesso!")
